40325c83422a09dc5254ac63378a2b5dfef378a4
JP <jnpaige@asu.edu>
2023-06-24 15:54:03 -0500

    added R script for initial data checking and exploration

---
+++ attribute analysis.R
@@ -0,0 +1,35 @@
+library(here)
+library(readxl)
+library(ggplot2)
+setwd(here::here())
+d<-read_xlsx("LBJ attribute data.xlsx", sheet=1)
+
+# Setting up appropriate factor level orders, making sure things are numeric, etc.
+
+head(d)
+pattern<-"EPA|MaxLength|MaxWidth|MaxThickness|TechLength|MaxTechWidth|MidThickness|PlatformWidth|PlatformThickness"
+d[,grep(pattern,names(d))]
+
+depths<-as.character(unique(sort(d$DepthStartcmbd, decreasing=FALSE)))
+
+d$DepthStartcmbd<-as.character(d$DepthStartcmbd)
+d$DepthStartcmbd<-factor(d$DepthStartcmbd, 
+                     levels = c(depths))
+
+d$CortexArea<-factor(d$CortexArea, 
+                         levels = c("0%","1-24%","25-49%","50-74%","75-99%","100%"))
+
+
+ggplot(data=d) +
+  geom_bar(aes(x=DepthStartcmbd, fill=EvidencePostDepBurning))
+
+ggplot(data=d) +
+  geom_bar(aes(x=DepthStartcmbd, fill=CortexArea))
+
+ggplot(data=d) +
+  geom_bar(aes(x=DepthStartcmbd, fill=PATINATION))
+
+ggplot(data=d)+
+  geom_point(aes(x=DepthStartcmbd, y=TechLength, color=CortexArea))
+
+
